f5cd1c99d4a268cbdfdd5a63d42daddee79df9a9
Nikolai Panich <nikolai.panich@swagmasha.org>
2024-03-13 18:47:31 +0300

    update lesson_1

--- svalka.py
+++ svalka.py
@@ -10,5 +10,14 @@
 # for i, m in enumerate(months, 1):  # начитать нумерацию с 1
 #    print(f'{i}: {m}')
 
-names = ['Bob', 'Alice', 'Guido']
-print(list(enumerate(names, 1)))
+# names = ['Bob', 'Alice', 'Guido']
+# print(list(enumerate(names, 1)))
+
+number = input('How old are you?')
+print('Your age is:' + number)
+print(f'Your age is: {number}')
+print('Your age is: {}'.format(str(number)))
+
+
+
+
