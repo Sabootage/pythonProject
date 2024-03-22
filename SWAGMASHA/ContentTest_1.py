daff86c9f3031168f047e07e18e68482a4f19d2c
Nikolai Panich <nikolai.panich@swagmasha.org>
2024-03-22 14:44:48 +0300

    update ContentTest_1.py

---
+++ SWAGMASHA/ContentTest_1.py
@@ -0,0 +1,111 @@
+from art import tprint
+
+tprint("SWAGMASHA")
+
+from colorama import init
+init()
+from colorama import Fore, Back, Style
+
+file = input(Fore.MAGENTA + 'Введите путь к файлу для проверки:' + Fore.RESET).strip('"')
+print(Style.RESET_ALL) #очистить цвет
+file2 = open(f'{file}', 'r', encoding='utf-8')
+file3 = file2.read() #готовая переменная для проверки
+# print(file3) #этот print вызывает печать всего документа построчно
+#Разбить file3 на отдельные строки
+file3 = file3.split('\n')
+# print(file3) #этот print печатает уже разбитый документ
+#вывести количестков строк в файле
+print('Количество строк в файле =', len(file3))
+#найти и псчитать количество строк, содержащих "//Paid"
+count = 0
+for line in file3:
+    if '//Paid' in line:
+        count += 1
+print('Количество строк, содержащих "//Paid" =', count)
+#найти и посчитать количество строк, содержащих "$price_"
+count2 = 0
+for line in file3:
+    if '$price_' in line:
+        count2 += 1
+print('Количество строк, содержащих "$price_" =', count2)
+if count == count2:
+    print(Fore.GREEN + 'Тест пройден успешно: есть все платные выборы')
+else:
+    print(Fore.RED + 'Возможно, пропущен платный выбор, пожалуйста, проверьте документ вручную')
+#найти и напечатать строки, содержашие "$price_1" с номером строки
+print(Style.RESET_ALL) #очистить цвет
+for line in file3:
+    if '$price_1' in line:
+        #вывести номер строки, содержащей "$price_1"
+        print(Fore.CYAN + 'Номер строки, содержащей "$price_1" =', file3.index(line))
+        print(Style.RESET_ALL + line)
+        #сравнить, что в строке '$price_1' содержатся '_price_1'
+        if '_price_1' in line:
+            print(Fore.GREEN + 'Тест пройден успешно')
+        elif '_price_1' not in line:
+            print(Fore.BLUE + 'Тег_1 отсутствует', Fore.GREEN + 'DONE')
+        else:
+            print(Fore.RED +'Тест провален')
+#найти и напечатать строки, содержашие "$price_2" с номером строки
+print(Style.RESET_ALL) #очистить цвет
+for line2 in file3:
+    if '$price_2' in line2:
+        #вывести номер строки, содержащей "$price_2"
+        print(Fore.CYAN + 'Номер строки, содержащей "$price_2" =', file3.index(line2))
+        print(Style.RESET_ALL + line2)
+        #сравнить, что в строке '$price_2' содержатся '_price_2'
+        if '_price_2' in line2:
+            print(Fore.GREEN + 'Тест пройден успешно')
+        elif '_price_2' not in line2:
+            print(Fore.BLUE + 'Тег_2 отсутствует', Fore.GREEN + 'DONE')
+        else:
+            print(Fore.RED +'Тест провален')
+#найти и напечатать строки, содержашие "$price_3" с номером строки
+print(Style.RESET_ALL) #очистить цвет
+for line3 in file3:
+    if '$price_3' in line3:
+        #вывести номер строки, содержащей "$price_3"
+        print(Fore.CYAN + 'Номер строки, содержащей "$price_3" =', file3.index(line3))
+        print(Style.RESET_ALL + line3)
+        #сравнить, что в строке '$price_3' содержатся '_price_3'
+        if '_price_3' in line3:
+            print(Fore.GREEN + 'Тест пройден успешно')
+        elif '_price_3' not in line3:
+            print(Fore.BLUE + 'Тег_3 отсутствует', Fore.GREEN + 'DONE')
+        else:
+            print(Fore.RED +'Тест провален')
+else:
+    print(Fore.RED + '$price_3 не найден')
+#Test_1 (проверка Online статуса)
+print(Style.RESET_ALL) #очистить цвет
+ch = int(input(Fore.MAGENTA + 'Пожалуйста, введите количество чаптеров в файле: ' + Fore.RESET))
+print(Style.RESET_ALL) #очистить цвет
+# посчитать количество 'CMD #statusOnline' в файле
+countOnline = file3.count('CMD #statusOnline')
+print('Количество "CMD #statusOnline" в файле =', countOnline)
+if countOnline == ch:
+    print(Fore.GREEN + 'Тест Online пройден успешно')
+else:
+    print(Fore.RED + 'Тест Online провален, вероятно, стоит проверить документ вручную')
+#Test_2 (проверка Inactive статуса)
+print(Style.RESET_ALL) #очистить цвет
+#посчитать количество 'CMD #statusInactive' в файле
+countInactive = file3.count('CMD #statusInactive=minutes:240')
+print('Количество "CMD #statusInactive" в файле =', countInactive)
+if countInactive == ch -1: #если количество "CMD #statusInactive" равно введенному колтичество чапетров, уменьшеному на 1
+    print(Fore.GREEN + 'Тест Inactive пройден успешно')
+else:
+    print(Fore.RED + 'Тест Inactive провален, вероятно, стоит проверить документ вручную')
+#Test_3 (проверка Offline статуса)
+print(Style.RESET_ALL) #очистить цвет
+if 'CMD #statusOffline' in file3:
+    #посчитать количество 'CMD #statusOffline' в файле
+    countOffline = file3.count('CMD #statusOffline')
+    print('Количество CMD #statusOffline =', countOffline)
+    if countOffline == 1:
+        print(Fore.GREEN + 'Тест Offline пройден успешно')
+    else:
+        print(Fore.RED + 'Тест Offline провален')
+else:
+    print('CMD #statusOffline не найдено')
+input(Fore.MAGENTA + 'Нажмите Enter для выхода\n')
--- helloApp/UI/main.qml
+++ helloApp/UI/main.qml
@@ -3,28 +3,14 @@ import QtQuick.Controls 2.15
 
 ApplicationWindow {
     visible: true
-    width: 400
-    height: 600
+    width: 600
+    height: 500
     title: "HelloApp"
 
-Rectangle {
-        anchors.fill: parent
-
-Image {
-            sourceSize.width: parent.width
-            sourceSize.height: parent.height
-            source: "./images/playas.jpg"
-            fillMode: Image.PreserveAspectCrop
-        }
-
-Rectangle {
-            anchors.fill: parent
-            color: "transparent"
-            Text {
-                text: "16:38:33"
-                font.pixelSize: 24
-                color: "black"
-            }
-        }
+    Text {
+        anchors.centerIn: parent
+        text: "Hello, World"
+        font.pixelSize: 24
     }
+
 }
